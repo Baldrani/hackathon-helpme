ea9816a17503ce0157880fc0dd92ccb7959c7a12
Baldrani <mael.mayon@free.fr>
2018-03-20 15:09:46 +0100

    Test

--- index.js
+++ index.js
@@ -10,21 +10,57 @@ express()
       console.log('test')
       res.setHeader('Content-Type', 'application/json');
       //res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
-      res.send(JSON.stringify({ 'speech': "coucou", 'displayText': "coucou" }));
+      res.send(JSON.stringify({
+          "speech": "this text is spoken out loud if the platform supports voice interactions",
+          "displayText": "this text is displayed visually",
+          "messages": {
+            "type": 1,
+            "title": "card title",
+            "subtitle": "card text",
+            "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+          },
+          "data": {
+            "google": {
+              "expectUserResponse": true,
+              "richResponse": {
+                "items": [
+                  {
+                    "simpleResponse": {
+                      "textToSpeech": "this is a simple response"
+                    }
+                  }
+                ]
+              }
+            },
+            "facebook": {
+              "text": "Hello, Facebook!"
+            },
+            "slack": {
+              "text": "This is a text response for Slack."
+            }
+          },
+          "contextOut": [
+            {
+              "name": "context name",
+              "lifespan": 5,
+              "parameters": {
+                "param": "param value"
+              }
+            }
+          ],
+          "source": "example.com",
+          "followupEvent": {
+            "name": "event name",
+            "parameters": {
+              "param": "param value"
+            }
+          }
+        }))
       //res.render('pages/index')
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 /*
-  callWeatherApi(city, date).then((output) => {
-     // Return the results of the weather API to Dialogflow
-
-   }).catch((error) => {
-     // If there is an error let the user know
-     res.setHeader('Content-Type', 'application/json');
-     res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
-   });
- };
  function callWeatherApi (city, date) {
    return new Promise((resolve, reject) => {
      // Create the path for the HTTP request to get the weather

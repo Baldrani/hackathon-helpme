8d5b538e755b461975b8348d7e7eb5b343677601
Baldrani <mael.mayon@free.fr>
2018-03-20 14:58:52 +0100

    Test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ express()
   .set('view engine', 'ejs')
   .get(['/','/test'], (req, res) => {
       console.log('test')
-      res.render('pages/index')
+      res.send(JSON.stringify({ 'speech': "coucou", 'displayText': "coucou" }));
+      //res.render('pages/index')
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

24425141732a3362d7c9903f9dd4b1cd7baee901
Baldrani <mael.mayon@free.fr>
2018-03-20 15:36:24 +0100

    Test

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ app.post('/webhook', (req, res) => {
     res.send(JSON.stringify({
         "speech": "this text is spoken out loud if the platform supports voice interactions",
         "displayText": "this text is displayed visually",
-        "messages": {
-          "type": 1,
-          "title": "card title",
-          "subtitle": "card text",
-          "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-      }}))
+        }))
 })
 
 app.use(express.static(path.join(__dirname, 'public')))

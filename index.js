16e5b8a0be8876a334199bb99d6fab3d1e1a3e95
Baldrani <mael.mayon@free.fr>
2018-03-20 14:52:09 +0100

    Test index.js

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get(['/','/test'], (req, res) =>
+  {
+      console.log('test')
+
+  })
+
+  /*res.render('pages/index'))*/
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
